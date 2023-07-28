3f0997fca43b940d4d164e93081d9cde5378465f
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-07-28 22:06:54 +0200

    modificata transizione da keyframes a transition

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const modifica = () => {
   if (window.scrollY > 400) {
-    document.getElementById("navBar").classList.add("navCambiacolore");
-    document.getElementById("navBar").classList.remove("colore");
-    document.getElementById("Bottone").classList.add("buttonchange");
-    document.getElementById("Bottone").classList.remove("buttonBase");
+    document.getElementById("navBar").classList.add("coloraNav");
+    document.getElementById("Bottone").classList.add("coloraButton");
   } else {
-    document.getElementById("navBar").classList.add("colore");
-    document.getElementById("navBar").classList.remove("navCambiacolore");
-    document.getElementById("Bottone").classList.add("buttonBase");
-    document.getElementById("Bottone").classList.remove("buttonChange");
+    document.getElementById("navBar").classList.remove("coloraNav");
+    document.getElementById("Bottone").classList.remove("coloraButton");
   }
   console.log(window.scrollY);
 };
 
 window.addEventListener("scroll", modifica);
 
-const svg = document.getElementById("svg");
+const svg = document.querySelectorAll("g[opacity='1']");
 console.log(svg);
+const hidden = [];
+let x;
+let time = setInterval(() => {
+  clearInterval(time);
+  x = Math.floor(Math.random() * 817);
+  svg[x].style.opacity = 0;
+  hidden.push(x);
+  if (hidden.length === 20) {
+    time = setInterval(() => {
+      clearInterval(time);
+
+      svg[x].style.opacity = 1;
+      x++;
+      if (hidden.length > x) return;
+    }, 100);
+  }
+  console.log(svg[x]);
+}, 100);
+x = 0;
